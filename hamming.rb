3aa8cc22bd46b123b34395bef02a0857786b58f5
Jordan Dale <Jordan.T.Dale@gmail.com>
2014-06-18 15:17:43 -0600

    The single greatest code ever developed by pair programming

--- hamming.rb
+++ hamming.rb
@@ -1,7 +1,21 @@
 require_relative "testing_library"
 
 def hamming(strand_1, strand_2)
-
+  hamming_distance = 0
+  if strand_1 >= strand_2
+    var_1 = strand_2
+    var_2 = strand_1
+  else
+    var_1 = strand_1
+    var_2 = strand_2
+  end
+  var_1.each_char do |letter|
+    x = var_1.index(letter)
+    if letter != var_2[x]
+      hamming_distance = hamming_distance + 1
+    end
+  end
+  hamming_distance
 end
 
 check("Hamming distance between identical strands",
